359dd197ba8a97d76ae8286c31e7567537163691
Gabe Faber <gmfaber55@gmail.com>
2023-01-24 11:53:22 -0700

    added code to dynamically render to the html dom, as well as added code to use day.js to get the time and added code to save user inputs to their local storage

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,88 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// prints to the header element the date by calling day.js and displays the month, day, year and seconds
+var currentDay = function () {
+  var todayHeader = dayjs();
+  $('#currentDay').text(todayHeader.format('MMM D, YYYY h:mm:ss a'));
+};
+// interval update for seconds
+setInterval(currentDay, 1000);
+//uses day.js to find out the time and gives us an hour
+var currentTime = parseInt(dayjs().format('H'))
 let time = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 
-const containerElements = ('#container')
+const containerElements = $('#container')
 
 function workHours() {
   //for loop
   for (let i = 0; i < time.length; i++) {
-    let row = $("<div class='row'>");
+    //creates a new html <div> element and sets the class to 'row'
+    var row = $("<div class='row'>");
+    row.attr('id', 'hour-' + [i + 9])
+
+    var colTimer = $("<div class='col-sm-2 col-md-1 py-3 hour text-center'>")
+
+    var dayTime = time[i] + 'AM'
+
+    if (time[i] >= 12) {
+      dayTime = time[i] + ' PM';
+      if (time[i] >= 13) {
+        dayTime = time[i] - 12 + ' PM';
+      }
+    }
+    colTimer.append(dayTime)
+
+    //var colMemo = $("<div class='col-8 col-md-10'>")
+
+    var textArea = $("<textarea class='col-8 col-md-10 description time-block' rows='3'>")
+    textArea.attr('id', 'hour-' + [i + 9])
+
+      .append(textArea)
+
+    //colMemo.append(textArea)
+
+    if (currentTime > time[i]) {
+      textArea.addClass('past')
+    }
+
+    if (currentTime == time[i]) {
+      textArea.addClass('present')
+    }
+
+    if (currentTime < time[i]) {
+      textArea.addClass('future')
+    }
+
+    let colSave = $("<div class='col 8 '>")
+    let button = $("<button>")
+    button.addClass("saveBtn btn fas fa-save col-sm-2 col-md-1 py-3")
+    button.attr('id', 'hour-' + [i + 9])
+    colSave.append(button)
+
+
+
+    containerElements.append(row.append(colTimer, textArea, button))
   }
+  $(".saveBtn").on("click", function () {
+    var textPlanner = $(this).siblings("textarea").val();
+    var timePlanner = $(this).parent().attr("id");
+    localStorage.setItem(timePlanner, textPlanner);
+  })
+
+
+  // here we are getting the local storage key and value pairs and setting the value to it. 
+  $("#hour-9").children("textarea").val(localStorage.getItem("hour-9"));
+  $("#hour-10").children("textarea").val(localStorage.getItem("hour-10"));
+  $("#hour-11").children("textarea").val(localStorage.getItem("hour-11"));
+  $("#hour-12").children("textarea").val(localStorage.getItem("hour-12"));
+  $("#hour-13").children("textarea").val(localStorage.getItem("hour-13"));
+  $("#hour-14").children("textarea").val(localStorage.getItem("hour-14"));
+  $("#hour-15").children("textarea").val(localStorage.getItem("hour-15"));
+  $("#hour-16").children("textarea").val(localStorage.getItem("hour-16"));
+  $("#hour-17").children("textarea").val(localStorage.getItem("hour-17"));
+
 }
 
-$(function () {
+workHours()
+/*$(function () {
 
 
 
@@ -45,3 +115,5 @@ $(function () {
   $("#hour-17").children("textarea").val(localStorage.getItem("hour-17"));
 
 });
+
+*/
